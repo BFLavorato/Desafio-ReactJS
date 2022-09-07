49e3d9864ec3ecebf8570355a193d1049f6207b9
Benjamin Lavorato <lavoratobenjamin@gmail.com>
2022-09-07 19:18:18 -0300

    Desafio NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+function NavBar () {
+    return(
+      <header>Mecenas Fight
+            <nav>
+              <ul>
+                <li><a href="">Guantes</a></li>
+                <li><a href="">Protecciones</a></li>
+                <li><a href="">Bolsas</a></li>
+                <li><a href="">Pads</a></li>
+              </ul>
+            </nav>
+          </header>
+          
+    );
+  }
+
+export default NavBar
